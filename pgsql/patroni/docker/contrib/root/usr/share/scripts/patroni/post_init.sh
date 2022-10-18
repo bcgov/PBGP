b893deec2d099bfb6896f73e0465035cbcbd573f
Karthikk Tamil Mani <70780157+karthikktamilmani-freshworks@users.noreply.github.com>
2022-10-18 15:10:07 -0700

    .

--- pgsql/patroni/docker/contrib/root/usr/share/scripts/patroni/post_init.sh
+++ pgsql/patroni/docker/contrib/root/usr/share/scripts/patroni/post_init.sh
@@ -7,10 +7,6 @@ if [[ (! -z "$APP_USER") &&  (! -z "$APP_PASSWORD") && (! -z "$APP_DATABASE")]];
 
   echo "Creating database ${APP_DATABASE}"
   psql "$1" -w -c "CREATE DATABASE ${APP_DATABASE} OWNER ${APP_USER} ENCODING '${APP_DB_ENCODING:-UTF8}' LC_COLLATE = '${APP_DB_LC_COLLATE:-en_US.UTF-8}' LC_CTYPE = '${APP_DB_LC_CTYPE:-en_US.UTF-8}'"
-
-  echo "Enabling PostGIS extension"
-  echo "\\c hcap \\\\ CREATE EXTENSION postgis;" | psql "$1"
-  echo "\\c hcap \\\\ CREATE EXTENSION pgcrypto;" | psql "$1"
 else
   echo "Skipping user creation"
   echo "Skipping database creation"
