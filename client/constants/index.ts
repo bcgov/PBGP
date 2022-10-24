15c7cfb08262c8854022f18935bcefe89a5d1771
Peter Pavlatos <pavlatos.peter@gmail.com>
2022-10-24 10:01:30 -0400

    GRANT-41 - formatted files altered

--- client/constants/index.ts
+++ client/constants/index.ts
@@ -1,4 +1,10 @@
 export * from './keycloak.constants';
 
-
-export const PlanningSteps = ['Facility Information', 'Funding Eligibility', 'Environmental / Climate Considerations', 'Funding and Project Cost Estimate Information', 'Support Documentation and Checklist', 'Authorization'];
+export const PlanningSteps = [
+  'Facility Information',
+  'Funding Eligibility',
+  'Environmental / Climate Considerations',
+  'Funding and Project Cost Estimate Information',
+  'Support Documentation and Checklist',
+  'Authorization',
+];
