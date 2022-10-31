a2a3911ace2a32154d183e3ac390c6d55c5af380
Leon <leon@freshworks.io>
2022-10-31 11:05:33 -0700

    Add Request Methods File

---
+++ client/constants/request-methods.ts
@@ -0,0 +1,19 @@
+export enum REQUEST_METHOD {
+    GET = 'get',
+    POST = 'post',
+    PATCH = 'patch',
+    PUT = 'put',
+    DELETE = 'delete',
+  }
+  
+  export const API_ENDPOINT = {
+    
+  };
+  
+//   export const AxiosPublic = axios.create({
+//     headers: {
+//       Accept: 'application/json',
+//       'Content-type': 'application/json',
+//     },
+//     baseURL: process.env.NEXT_PUBLIC_API_URL,
+//   });
