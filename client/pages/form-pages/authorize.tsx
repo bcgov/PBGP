1a3355a4f6abd7089767e9a8b36fb9847d7dc88c
Peter Pavlatos <pavlatos.peter@gmail.com>
2022-11-01 10:06:22 -0400

    GRANT-69 - add signature canvas package to allow user to sign application

--- client/pages/form-pages/authorize.tsx
+++ client/pages/form-pages/authorize.tsx
@@ -1,12 +1,63 @@
+import React, { useState, useRef } from 'react';
 import { Form, Formik, FormikProps } from 'formik';
-import { Field, FormStepTitles, FormSteps } from '@components';
+import { Field, FormStepTitles, FormSteps, Button } from '@components';
 import { AuthorizationInterface } from 'constants/interfaces';
+import SignaturePad from 'react-signature-canvas';
+import SignatureCanvas from 'react-signature-canvas';
 
 const initialValues = {
   projectManagerSignature: '',
   financialOfficerSignature: '',
 };
- 
+
+const SignPad: React.FC<any> = () => {
+  const [dataURL, setDataURL] = useState<string | null>(null);
+  const [showSign, setShowSign] = useState<boolean | null>(false);
+  let padRef = useRef<SignatureCanvas>(null);
+  const clear = () => {
+    padRef.current?.clear();
+  };
+  const trim = () => {
+    const url = padRef.current?.getTrimmedCanvas().toDataURL('image/png');
+    if (url) setDataURL(url);
+    setShowSign(false);
+  };
+  return (
+    <div className='w-full'>
+      {dataURL ? (
+        <div className='w-full flex justify-between p-4'>
+          <img className={'sigImage'} src={dataURL} alt='user generated signature' />
+        </div>
+      ) : null}
+      {showSign && (
+        <SignaturePad
+          ref={padRef}
+          canvasProps={{ className: 'sigCanvas w-full border-2 mb-2 border-gray-200' }}
+        />
+      )}
+      <div className='sigPreview flex justify-between'>
+        {!showSign ? (
+          <>
+            <div></div>
+            <Button variant='outline' onClick={() => setShowSign(true)}>
+              Sign
+            </Button>
+          </>
+        ) : (
+          <>
+            <Button variant='outline' onClick={clear}>
+              Clear
+            </Button>
+            <Button variant='primary' onClick={trim}>
+              Save
+            </Button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
 export const AuthorizationPage: React.FC = () => {
   return (
     <Formik initialValues={initialValues} onSubmit={() => {}}>
@@ -32,13 +83,15 @@ export const AuthorizationPage: React.FC = () => {
               label='Project Manager, Name and Signature'
               maxLength={300}
             ></Field>
+            <SignPad />
+
             <Field
               name='financialOfficerSignature'
               type='text'
               label='Financial Officer, Name and Signature'
               maxLength={300}
             ></Field>
-
+            <SignPad />
             <div className='flex flex-col-1 w-full space-x-4'>
               <span className='flex-1'>
                 <Field
