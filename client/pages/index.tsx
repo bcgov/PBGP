9d0d7dd741012d12c9e52bda6e1726e9f0af8918
Peter Pavlatos <pavlatos.peter@gmail.com>
2022-11-10 17:04:41 -0500

    GRANT-107 - Added more table columns. Removed unused code

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -21,12 +21,14 @@ const Dashboard: NextPage = () => {
   // };
   return (
     <div className='flex flex-col w-full px-10 py-5 bg-white'>
-      <PageTitle
+      {/* <PageTitle
         title={`BC Air Access Program Application`}
         description={`Learn more about eligibility, prepare documents and deadline of the program, please click here`}
       />
-      {/* <Stepper steps={PlanningSteps} currentStep={currentStep} /> */}
+      <Stepper steps={PlanningSteps} currentStep={currentStep} /> */}
+
       <FormContent step={currentStep} formTitle={Object.keys(FormSteps)[currentStep - 1]} />
+      
       {/* <div className='flex-1 flex flex-col min-h-0'>
         <div className='flex justify-center'>
           <div className='w-2/4 p-4 gap-y-6 bg-white flex justify-between'>
