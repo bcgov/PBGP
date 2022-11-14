1c8a2764b90655eaf954db4019ece5cff32040fd
Karthikk Tamil Mani <70780157+karthikktamilmani-freshworks@users.noreply.github.com>
2022-11-14 08:59:10 -0800

    Grant 120 dockerize prod build (#25)
    * Dockerize apps for production
    
    * Fix healthcheck endpoint
    
    * Openshift commands

--- client/constants/keycloak.constants.ts
+++
@@ -1,5 +0,0 @@
-export const keycloakConfig = {
-  url: process.env.NEXT_PUBLIC_KC_AUTH_URL || '',
-  realm: process.env.NEXT_PUBLIC_KC_AUTH_REALM || '',
-  clientId: process.env.NEXT_PUBLIC_KC_AUTH_CLIENT_ID || '',
-};
--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: false, // https://github.com/react-keycloak/react-keycloak/issues/182#issuecomment-1119370025 Strict mode is placed within KC provider
+    publicRuntimeConfig: {
+        NEXT_PUBLIC_REDIRECT_URI: process.env.NEXT_PUBLIC_REDIRECT_URI,
+        NEXT_PUBLIC_KC_AUTH_URL: process.env.NEXT_PUBLIC_KC_AUTH_URL,
+        NEXT_PUBLIC_KC_AUTH_REALM: process.env.NEXT_PUBLIC_KC_AUTH_REALM,
+        NEXT_PUBLIC_KC_AUTH_CLIENT_ID: process.env.NEXT_PUBLIC_KC_AUTH_CLIENT_ID,
+        NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+    },
+    reactStrictMode: false, // https://github.com/react-keycloak/react-keycloak/issues/182#issuecomment-1119370025 Strict mode is placed within KC provider
 };
