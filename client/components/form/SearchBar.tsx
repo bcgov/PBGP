4cf9f03c2128d02d5d5e9a055a5169140b57f6a1
Peter Pavlatos <pavlatos.peter@gmail.com>
2022-11-17 12:16:50 -0500

    GRANT-75 - Added new folder to client for application dashboard and single application page

--- client/components/form/SearchBar.tsx
+++ client/components/form/SearchBar.tsx
@@ -12,7 +12,7 @@ export const SearchBar = ({
       <input
         type='text'
         id='simple-search'
-        className='bg-gray-50 rounded border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full pr-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+        className='bg-white rounded border border-gray-300 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full pr-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
         placeholder={placeholderText}
         onChange={handleChange}
         required
