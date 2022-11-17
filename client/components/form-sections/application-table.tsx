434c02ba80a2e9428ab31320790e47b886ee426e
Peter Pavlatos <pavlatos.peter@gmail.com>
2022-11-17 12:28:15 -0500

    GRANT-75 - Removed from Dashboard page into seperate component

--- client/components/form-sections/application-table.tsx
+++ client/components/form-sections/application-table.tsx
@@ -1,4 +1,4 @@
-import { Button, SearchBar } from '@components';
+import { Button, SearchBar, ApplicationTable } from '@components';
 import { Pagination } from '../form';
 import { useEffect, useState } from 'react';
 import { ApplicationTableData } from '../../constants';
@@ -6,78 +6,6 @@ import { ApplicationDataInterface } from '../../constants/interfaces';
 import axios from 'axios';
 
 type Props = { applications: ApplicationDataInterface[] };
-
-const TableHeader: React.FC = () => {
-  const headers = [
-    'Confirmation ID',
-    'Facility',
-    'Project',
-    'Estimated Cost',
-    'Asks',
-    'Assigned to',
-    'Last update',
-    'Status',
-  ];
-  const tdStyles =
-    'table-td table-header px-6 py-4 text-left text-sm font-strong border-b-2  border-bcYellowWarning';
-  return (
-    <thead className='border-b bg-bcGrayInput table-header'>
-      <tr>
-        {headers &&
-          headers.map((title: string, index: number) => (
-            <th key={`th${index}`} className={tdStyles}>
-              {title}
-            </th>
-          ))}
-      </tr>
-    </thead>
-  );
-};
-
-const TableBody: React.FC<Props> = applications => {
-  const handleSelectRow = (index: number) => {
-    alert(index);
-  };
-  const tdStyles =
-    'table-td px-6 py-4 text-left text-sm font-strong flexitems-center justify-between';
-  return (
-    <tbody>
-      {applications.applications &&
-        applications.applications.map((row: any, index: number) => (
-          <tr
-            key={`row${index}`}
-            onClick={() => handleSelectRow(row.BCAAP_Form_ID)}
-            className='bg-white border-b-2 even:bg-bcGrayInput
-              border-gray-200'
-          >
-            <td className={tdStyles}>{row.Confirmation_ID}</td>
-            <td className={tdStyles}>{row.Facility_Name}</td>
-            <td className={tdStyles}>{row.Assigned_To}</td>
-            <td className={tdStyles}>{row.Created_At}</td>
-            <td className={tdStyles}>{row.Status}</td>
-            <td className={tdStyles}>{row.Assigned_To}</td>
-            <td className={tdStyles}>{row.Updated_At}</td>
-            <td className={tdStyles}>{row.Status}</td>
-          </tr>
-        ))}
-    </tbody>
-  );
-};
-
-const ApplicationTable: React.FC<Props> = applications => {
-  return (
-    <div>
-      <table className='min-w-full text-center'>
-        <TableHeader />
-        {ApplicationTableData && applications.applications.length != 0 ? (
-          <TableBody applications={applications.applications} />
-        ) : (
-          <p className='text-center text-sm mt-4'>No applications found.</p>
-        )}
-      </table>
-    </div>
-  );
-};
 
 const FilterBy: React.FC<Props> = applications => {
   return (
