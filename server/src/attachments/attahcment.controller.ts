ce556cb43b93de3d0ffb15a1193e02908bed52c0
Leon <leon@freshworks.io>
2022-12-15 13:41:48 -0800

    Make File Downloadable

--- server/src/attachments/attahcment.controller.ts
+++ server/src/attachments/attahcment.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import { PublicRoute } from '@/common/decorator';
+import { Controller, Get, Param, Res, StreamableFile } from '@nestjs/common';
 import { Response } from 'express';
+import { Readable } from 'stream';
 import { Attachment } from './attachment.entity';
 import { AttachmentService } from './attachment.service';
 
@@ -8,15 +10,19 @@ export class AttachmentController {
   constructor(private attachmentService: AttachmentService) {}
 
   @Get('/:id')
-  async getAttachment(@Res() res: Response, @Param('attachmentId') attachmentId: string) {
+  async getAttachment(
+    @Res({ passthrough: true }) res: Response,
+    @Param('attachmentId') attachmentId: string
+  ) {
     const attachment: Attachment = await this.attachmentService.getAttachment(attachmentId);
     if (attachment) {
-      return res
-        .set({
-          'content-disposition': `attachment; filename=${attachment.originalName}`,
-          'Content-Type': 'application/pdf',
-        })
-        .json(attachment.data);
+      const file = Readable.from(attachment.data);
+
+      res.set({
+        'content-disposition': `attachment; filename=${attachment.originalName}`,
+      });
+
+      return new StreamableFile(file);
     }
     return;
   }
