ef5e18b3219e74bcccd3bbf566abd6ccaa71768c
Leon <leon@freshworks.io>
2022-12-19 14:31:10 -0800

    Address PR Feedback #1

--- server/src/score/score.entity.ts
+++ server/src/score/score.entity.ts
@@ -1,89 +1,14 @@
-import { Application } from '@/application/application.entity';
-import { User } from '@/user/user.entity';
+import { Application } from '../application/application.entity';
+import { ScoreBaseEntity } from '../common/score-base.entity';
+import { User } from '../user/user.entity';
 import { Entity, Column, PrimaryGeneratedColumn, OneToOne, JoinColumn } from 'typeorm';
-import { CustomBaseEntity } from '../common/custom-base.entity';
 
 @Entity({
   name: 'pbgp_score',
 })
-export class Score extends CustomBaseEntity {
+export class Score extends ScoreBaseEntity {
   @PrimaryGeneratedColumn('uuid', { name: 'score_id' })
   id: string;
-
-  @Column({ type: 'int', nullable: true })
-  projectTypeScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  projectNeedScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  projectFundingScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  pastBcaapFundingScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  facilityMasterPlanScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  facilityUsageScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  trafficDataScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  climatePerspectiveScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  climateBestPracticesScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  environmentalRisksScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  environmentalInnovationScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  projectDescriptionScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  climateGoalsScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  organizationClimateGoalScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  successMeasurementScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  safetyScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  medevacScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  localBenefitsScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  longTermScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  communitySupportScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  concernsScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  contingencyPlanScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  classBCostScore: number;
-
-  @Column({ type: 'int', nullable: true })
-  thirdPartyContributionScore: number;
-
-  @Column({ type: 'varchar', length: '2000', nullable: true })
-  overallComments: string;
 
   @OneToOne(() => User)
   @JoinColumn()
