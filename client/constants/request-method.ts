025be0e703851211ba4f48895ea3324dadb36b09
Peter Pavlatos <pavlatos.peter@gmail.com>
2023-01-05 11:55:20 -0500

    GRANT-89 - resolved format issues in 2 files

--- client/constants/request-method.ts
+++ client/constants/request-method.ts
@@ -16,6 +16,7 @@ export const API_ENDPOINT = {
   getApplicationStatus: (applicationId: string) => `/applications/${applicationId}/status`,
   getApplicationEvaluator: (applicationId: string) => `/applications/${applicationId}/assign`,
   getApplicationScores: (applicationId: string) => `/applications/${applicationId}/scores`,
-  updateApplicationScores:  (applicationId: string, scoreId: string) => `/applications/${applicationId}/scores/${scoreId}`,
+  updateApplicationScores: (applicationId: string, scoreId: string) =>
+    `/applications/${applicationId}/scores/${scoreId}`,
   getApplicationAttachments: (id: string) => `attachments/${id}`,
 };
