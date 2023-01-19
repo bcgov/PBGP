afac8acad2b49262c08501ec2a8152b04709573e
Karthikk Tamil Mani <70780157+karthikktamilmani-freshworks@users.noreply.github.com>
2023-01-19 11:59:36 -0800

    Lint!

--- client/next.config.js
+++ client/next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    publicRuntimeConfig: {
-        NEXT_PUBLIC_REDIRECT_URI: process.env.NEXT_PUBLIC_REDIRECT_URI,
-        NEXT_PUBLIC_KC_AUTH_URL: process.env.NEXT_PUBLIC_KC_AUTH_URL,
-        NEXT_PUBLIC_KC_AUTH_REALM: process.env.NEXT_PUBLIC_KC_AUTH_REALM,
-        NEXT_PUBLIC_KC_AUTH_CLIENT_ID: process.env.NEXT_PUBLIC_KC_AUTH_CLIENT_ID,
-        NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
-        NEXT_PUBLIC_LARGE_PROJECT: process.env.NEXT_PUBLIC_LARGE_PROJECT,
-        NEXT_PUBLIC_SMALL_PROJECT: process.env.NEXT_PUBLIC_SMALL_PROJECT,
-        NEXT_PUBLIC_ENVIRONMENT_PLANNING: process.env.NEXT_PUBLIC_ENVIRONMENT_PLANNING,
-        NEXT_PUBLIC_DEVELOPMENT_PLANNING: process.env.NEXT_PUBLIC_DEVELOPMENT_PLANNING,
-    },
-    reactStrictMode: false, // https://github.com/react-keycloak/react-keycloak/issues/182#issuecomment-1119370025 Strict mode is placed within KC provider
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_REDIRECT_URI: process.env.NEXT_PUBLIC_REDIRECT_URI,
+    NEXT_PUBLIC_KC_AUTH_URL: process.env.NEXT_PUBLIC_KC_AUTH_URL,
+    NEXT_PUBLIC_KC_AUTH_REALM: process.env.NEXT_PUBLIC_KC_AUTH_REALM,
+    NEXT_PUBLIC_KC_AUTH_CLIENT_ID: process.env.NEXT_PUBLIC_KC_AUTH_CLIENT_ID,
+    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+    NEXT_PUBLIC_LARGE_PROJECT: process.env.NEXT_PUBLIC_LARGE_PROJECT,
+    NEXT_PUBLIC_SMALL_PROJECT: process.env.NEXT_PUBLIC_SMALL_PROJECT,
+    NEXT_PUBLIC_ENVIRONMENT_PLANNING: process.env.NEXT_PUBLIC_ENVIRONMENT_PLANNING,
+    NEXT_PUBLIC_DEVELOPMENT_PLANNING: process.env.NEXT_PUBLIC_DEVELOPMENT_PLANNING,
+  },
+  reactStrictMode: false, // https://github.com/react-keycloak/react-keycloak/issues/182#issuecomment-1119370025 Strict mode is placed within KC provider
 };
