38657200ffd74f9ce596976cdb27ffd67e097882
Leon <leon@freshworks.io>
2023-01-19 12:16:04 -0800

    Remove Puppeteer as It's Incompatible with Docker

--- server/src/pdf-export/scripts/workshop-to-pdf.service.ts
+++ server/src/pdf-export/scripts/workshop-to-pdf.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
-// const fs = require('fs');
-// const path = require('path');
-// const puppeteer = require('puppeteer');
+const fs = require('fs');
+const path = require('path');
+// const puppeteer = require('puppeteer'); // Puppeteer blows up docker :(
 // const handlebars = require('handlebars');
 
 @Injectable()
 export class WorkshopToPdfService {
-  constructor() {}
+  HTML_TEMPLATE_FILE_PATH: string;
+  constructor() {
+    this.HTML_TEMPLATE_FILE_PATH = 'public/workshop-score-pdf-template.html';
+  }
 
-  workshopDataToPdf(): void {
-    // console.log('Test');
+  async workshopDataToPdf(): Promise<void> {
+    const templateHtml = fs.readFileSync(
+      path.join(process.cwd(), this.HTML_TEMPLATE_FILE_PATH),
+      'utf8'
+    );
+    console.log(templateHtml);
   }
 }
