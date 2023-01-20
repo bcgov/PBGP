fe1ff535336108ae6aa4cb3e1e4dfc6d7a99e020
Leon <leon@freshworks.io>
2023-01-20 08:56:39 -0800

    Setup Handlebars & Sample Data

--- server/src/pdf-export/scripts/workshop-to-pdf.service.ts
+++ server/src/pdf-export/scripts/workshop-to-pdf.service.ts
@@ -3,13 +3,19 @@ import { Injectable } from '@nestjs/common';
 const fs = require('fs');
 const path = require('path');
 // const puppeteer = require('puppeteer'); // Puppeteer blows up docker :(
-// const handlebars = require('handlebars');
+const handlebars = require('handlebars');
 
 @Injectable()
 export class WorkshopToPdfService {
   HTML_TEMPLATE_FILE_PATH: string;
+  SAMPLE_DATA_PATH: string;
+  WORKSHOP_DATA: object;
   constructor() {
     this.HTML_TEMPLATE_FILE_PATH = 'public/workshop-score-pdf-template.html';
+    this.SAMPLE_DATA_PATH = 'public/sample-workshop-data.json';
+    this.WORKSHOP_DATA = JSON.parse(
+      fs.readFileSync(path.join(process.cwd(), this.SAMPLE_DATA_PATH), 'utf8')
+    );
   }
 
   async workshopDataToPdf(): Promise<void> {
@@ -17,6 +23,9 @@ export class WorkshopToPdfService {
       path.join(process.cwd(), this.HTML_TEMPLATE_FILE_PATH),
       'utf8'
     );
-    console.log(templateHtml);
+
+    var template = handlebars.compile(templateHtml);
+    var html = template(this.WORKSHOP_DATA);
+    console.log(html);
   }
 }
